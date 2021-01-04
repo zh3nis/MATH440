c1339916e8b4e677beac455aace0fc65a57372b4
Zhenisbek Assylbekov <zhassylbekov@nu.edu.kz>
2021-01-04 17:05:45 +0600

    Create poverty.R

---
+++ chp01/poverty.R
@@ -0,0 +1,2 @@
+poverty = read.table("https://raw.githubusercontent.com/OpenIntroStat/openintro-statistics-slides/master/Chp%208/8-2_least_square_reg/figures/poverty/poverty.txt", h = T, sep = "\t")
+lm(Poverty ~ Graduates, data=poverty)
